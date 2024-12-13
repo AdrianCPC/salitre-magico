e4ca6e569df4ee4f15a353a1f278890a6ee77bb2
AdrianPC <adriancamilopc@gmail.com>
2024-12-13 17:09:13 -0500

    Logica de validaciones de edad y estatura

--- parque_salitre/parque/forms.py
+++ parque_salitre/parque/forms.py
@@ -1,28 +1,49 @@
 """Creación de formularios"""
 from django import forms
+from django.core.exceptions import ValidationError
 from .models import Cliente, Empleado, Atraccion, Tiquete
 
 #Creacion de los formularios
 class ClienteForm(forms.ModelForm):
     """Formulario Cliente"""
     class Meta:
+        """Campos cliente"""
         model = Cliente
         fields = '__all__' #Inclusion de todos los campos en modelo cliente
+    def clean_edad(self):
+        """Edad escrita correctamente"""
+        edad = self.cleaned_data.get('edad')
+        if edad is None:
+            raise ValidationError("La edad es obligatoria.")
+        if edad < 0:
+            raise ValidationError("La edad no puede ser negativa.")
+        return edad
+    def clean(self):
+        cleaned_data = super().clean()
+        edad = cleaned_data.get('edad')
+        contacto_familiar = cleaned_data.get('contacto_familiar')
+        if edad is not None and edad < 18 and not contacto_familiar:
+            raise forms.ValidationError(
+                "El contacto familiar es obligatorio para menores de 18 años")
+        return cleaned_data
 
 class EmpleadoForm(forms.ModelForm):
     """Formulario Empleado"""
     class Meta:
+        """Campos empleado"""
         model = Empleado
         fields = '__all__'
 
 class AtraccionForm(forms.ModelForm):
     """Formulario Atraccion"""
     class Meta:
+        """Campos atraccion"""
         model = Atraccion
         fields = '__all__'
 
 class TiqueteForm(forms.ModelForm):
     """Formulario Tiquete"""
     class Meta:
+        """Campos tiquetes"""
         model = Tiquete
         fields = '__all__'
--- parque_salitre/parque/models.py
+++ parque_salitre/parque/models.py
@@ -1,5 +1,6 @@
 """Creacion de modelos"""
-
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.core.exceptions import ValidationError
 from django.db import models
 
 
@@ -9,13 +10,23 @@ class Cliente(models.Model):
     cedula = models.CharField(max_length=15, unique=True)
     telefono = models.CharField(max_length=15)
     correo = models.EmailField()
-    estatura = models.FloatField()
-    edad = models.IntegerField()
+    estatura = models.FloatField(validators=[
+        MinValueValidator(0, message="La estatura debe ser positiva.")
+    ])
+    edad = models.IntegerField(validators=[
+        MinValueValidator(0, message="La edad no puede ser negativa."),
+        ( MaxValueValidator(110, message="Edad no válida."))
+    ])
     contacto_familiar = models.CharField(max_length=100, null=True, blank=True)
     publicidad = models.BooleanField(default=True)
     def __str__(self):
         return str(self.nombre)
-
+    def clean(self):
+        """Validación edad negativa"""
+        if self.edad is not None:
+            if self.edad < 0:
+                raise ValidationError("La edad no puede ser negativa")
+        super().clean()
 
 class Empleado(models.Model):
     """Tipos empleados en el parque"""
@@ -41,7 +52,9 @@ class Atraccion(models.Model):
     nombre = models.CharField(max_length=100)
     descripcion = models.TextField()
     clasificacion = models.CharField(max_length=50)
-    estatura_minima = models.FloatField()
+    estatura_minima = models.FloatField(validators=[
+        MinValueValidator(0, message="La estatura mínima debe ser positiva.")
+    ])
     condiciones_uso = models.TextField()
     estado = models.BooleanField(default=True) # True: Disponible, False: No disponible
     def __str__(self):
